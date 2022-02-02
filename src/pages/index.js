99acd1562c909d00956164138d3b79692a2e0647
joão Paulo <ajeypow@gmail.com>
2022-02-02 17:28:24 -0300

    comments index.js and about.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
+// first step ever time to creating nice new page 
+//command to remenber "imr + tab"
 import * as React from "react";
 
+// Step 2 : define your componet
+// command to remenber "rafc + tab"
 //homepage 
 const Greeting  = () => {
   return (
@@ -8,5 +12,5 @@ const Greeting  = () => {
   </div>
   );
 };
-
+ // step 3 : export yout component
 export default Greeting; 
