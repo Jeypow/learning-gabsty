c4115b5567f1f2cb685d0dc31e7e7ca3741c6511
joão Paulo <ajeypow@gmail.com>
2022-02-04 01:13:01 -0300

    creating page terceira

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,19 @@
 // first step ever time to creating nice new page 
 //command to remenber "imr + tab"
 import * as React from "react";
-
+import {Link} from 'gatsby';
 // Step 2 : define your componet
-// command to remenber "rafc + tab"
+// command to remenber "Rafc + tab"
 //homepage 
 const Greeting  = () => {
   return (
     <main>
       <title>Home Page</title>
          <h1>ohayo gozaimasu</h1>
+         <Link to = "/terceira">page one</Link>
          <p>salve marcos!</p>
          </main>
   );
 };
  // step 3 : export your component
-export default Greeting; 
+export default Greeting ; 
---
+++ src/pages/terceira.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import {Link} from "gatsby";
+
+const terceira = () => {
+  return (
+        <main>
+          <title>
+          Page one to link gatsby package
+          </title>
+          <h1> arorrodo</h1>
+          <Link to ="/">Home</Link>
+          <p>Perdeu!</p>
+        </main>
+        );  
+};
+ 
+export default terceira;
