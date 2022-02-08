959f3142ff0ef6d8d3b470c912e7609d39fbee16
joão Paulo <ajeypow@gmail.com>
2022-02-08 02:09:07 -0300

    use data do shared information

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Layout from '../components/Layout';
+
+
+const blog = () => {
+  return (
+      <Layout pageTitle= "My Blog Posts">
+          <p>My cool post will go in here</p>
+      </Layout>
+  )
+};
+
+export default blog;
